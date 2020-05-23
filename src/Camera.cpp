17f13d829dd5252cf74b1a8d5f1525aa85c658a4
rishikawa <ishikawa@cvl.iis.u-tokyo.ac.jp>
2020-05-23 11:33:26 +0900

    set mask

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -2,7 +2,15 @@
 
 void camera::imwrite() {
 	cv::Mat colorimage = cv::Mat(cv::Size(viewWidth_, viewHeight_), CV_8UC3);
-	memcpy(colorimage.data, getColorData(), sizeof(uchar)* 3 * colorimage.size().width*colorimage.size().height);
+	memcpy(colorimage.data, getColorData(), sizeof(uchar) * 3 * colorimage.size().width*colorimage.size().height);
+	if (getType() == FISHEYE) {
+		cv::Mat mask = cv::Mat::zeros(cv::Size(viewWidth_, viewHeight_), CV_8UC3);
+		cv::Mat dst = cv::Mat::zeros(cv::Size(viewWidth_, viewHeight_), CV_8UC3);
+		cv::ellipse(mask, cv::RotatedRect(cv::Point2f(viewWidth_ / 2, viewHeight_ / 2),cv::Size2f(viewWidth_, viewHeight_),0),cv::Scalar(255.0,255.0,255.0),-1);
+		//cv::circle(colorimage, cv::Point2f(viewWidth_ / 2, viewHeight_ / 2), viewWidth_ / 2, cv::Scalar(0.0, 0.0, 0.0),-1);
+		colorimage.copyTo(dst, mask);
+		colorimage = dst;
+	}
 	cv::imwrite(fileBase+std::to_string(count) +".jpg", colorimage);
 	count++;
 }
